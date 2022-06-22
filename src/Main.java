0526267acc1951c945ad154a3e575be9d8351cb2
Денис Морозов <den.morozov.94@list.ru>
2022-06-22 12:17:17 +0300

    added dz2

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args){
+        //входные данные
+        int price = 350;
+        int bun = 20;
+        // логика программы
+        int bonus = price / bun;
+        System.out.println("Ваш бонус составляет " + bonus + " бесплатных миль");
+    }
+
+
+}
